7e868adc43e32da3d37f195bdcb946825664dda0
Oscar Otero <oskar.oc@gmail.com>
2022-03-31 20:18:19 +0200

    Create playRound function in javascript.js
    Create a function where user play and computer play are compared
    to decide the winner and it returns a message depending on the
    winner.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,25 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "You tied!";
+    }
+}
+
+let playerSelection = "rock";
+let computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection));
