07c930947c6490cdafb8c353a71d87389e7a2174
Oscar Otero <oskar.oc@gmail.com>
2022-03-31 20:48:14 +0200

    Create game function
    Create a game function where the game is played for 5 rounds.
    There is a score and the final winner is shown in console.
    Modify playRound function to incorporate game score.

--- javascript.js
+++ javascript.js
@@ -16,22 +16,45 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === "Rock" && computerSelection === "Paper") {
+        ++computerScore;
         return "You Lose! Paper beats Rock";
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        ++playerScore;
         return "You Win! Rock beats Scissors"
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        ++playerScore;
         return "You Win! Paper beats Rock";
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        ++computerScore;
         return "You Lose! Scissors beats Paper";
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        ++computerScore;
         return "You Lose! Rock beats Scissors";
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        ++playerScore;
         return "You Win! Scissors beats Paper";
     } else {
-        return "You tied!";
+        return "It is a tie!";
     }
 }
 
-let playerSelection = "rock";
-let computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please, make your choice:");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Your score is: ${playerScore}`);
+        console.log(`Computer score is: ${computerScore}`);
+    }
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You lost the game");
+    } else {
+        console.log("It is a tie!");
+    }
+}
+
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+// console.log(playRound(playerSelection, computerSelection));
